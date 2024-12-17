ecf7cd0bc00db93a249c80b11a54b05450a9d023
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-12-17 21:50:50 +0100

    created handleDeletePost function and  handleDeleteClick

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,5 +1,7 @@
 import style from "./Card.module.css";
-function CardComponent({ post }) {
+
+function CardComponent({ post, removePost }) {
+    const handleDeleteClick = () => { removePost(post.id) }
     //console.log(post);
     return (
         <>
@@ -8,8 +10,8 @@ function CardComponent({ post }) {
                 <div className="card-body">
                     <h5 className="card-title">{post.title}</h5>
                     {/* <p className="card-text">{post.content}</p>  */}
-                    <a href="#" className="read-btn">Leggi di più</a>
-                </div>
+                    <a href="#" className="btn read-btn">Leggi di più</a>
+                    <button type="button" className="btn btn-danger mx-3" onClick={handleDeleteClick}>Elimina post</button>                </div>
             </div >
         </>
     )
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -11,16 +11,21 @@ function MainComponent() {
     function handleNewPost(newPost) {
         setPosts([...posts, newPost])
     }
+
+    function handleDeletePost(id) {
+        const updatedPostList = posts.filter(post => post.id !== id);
+        setPosts(updatedPostList);
+    }
     // struttura del main component
     return (
         <main className="container">
             <section className="d-flex justify-content-center flex-wrap align-items-center">
                 {posts.map((post) => (
-                    post.published ? <CardComponent key={post.id} post={post} /> : null
+                    post.published ? <CardComponent key={post.id} post={post} removePost={handleDeletePost} /> : null
                 ))}
             </section>
             {/*includo il form e gli passo la funzione per inviare il nuovo post al component padre, il main */}
-            <PostForm addPost={handleNewPost} />
+            <PostForm addPost={handleNewPost} removePost={handleDeletePost} />
         </main>
     )
 }
